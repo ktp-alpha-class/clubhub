3cd46bc79e3fa565af50f0437943ef88689984b7
arminapr <arminaparva@gmail.com>
2024-10-27 21:39:31 -0400

    clean up

--- frontend/src/pages/calendar/index.tsx
+++ frontend/src/pages/calendar/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
 import Calendar from '../../components/Calendar';
-import styles from '../../styles/Calendar.module.css';
 
 const ClubCalendar: React.FC = () => {
   return (
