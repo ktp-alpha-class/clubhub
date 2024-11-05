eca5262ca75ff739f60f2772960034a9cc0ee394
cooper-ke <cooper.ke@northeastern.edu>
2024-11-04 19:43:53 -0500

    Added user.py route and updated table to match

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -3,4 +3,8 @@ from supabase import Client
 
 # Put routes for users (profile, following) here
 def UserRoutes(app: Flask, supabase: Client):
-    pass
+    # Get user profile data
+    @app.route("/user")
+    def get_examples():
+        profile_data = supabase.table("User").select("username").execute()
+        return jsonify(profile_data.data)
