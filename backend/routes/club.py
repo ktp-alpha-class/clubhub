d8edcad5dd6acc7f87205e882d6c1aabb40ac1be
quinncahill <cahillquinn23@gmail.com>
2024-11-06 18:37:35 -0500

    updating the club

--- backend/routes/club.py
+++ backend/routes/club.py
@@ -4,3 +4,26 @@ from supabase import Client
 # Put general club routes here (info, no links or events stuff)
 def ClubRoutes(app: Flask, supabase: Client):
     pass
+
+    @app.route("/club/<club_id>", methods=["PUT"])
+    @authenticate_club_admin(supabase)
+    def update_event(club_id):
+        try:
+            data = request.json
+
+            if not data:
+                return jsonify({"error": "No data"}), 400
+
+            response = (
+                supabase.table("club")
+                .update(data)
+                .eq("club_id", club_id)
+                .execute()
+            )
+
+            if not response.data:
+                return jsonify({"error": "Data not found"}), 404
+
+            return jsonify({"message": "Event updated successfully", "updated_club": response.data})
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
