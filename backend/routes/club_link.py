6b9caab4a35f356decedd013c021296437e94f83
aylaurquizo <urquizo.a@northeastern.edu>
2024-11-06 18:17:29 -0500

    added auth_id as a parameter

--- backend/routes/club_link.py
+++ backend/routes/club_link.py
@@ -7,7 +7,7 @@ def ClubLinkRoutes(app: Flask, supabase: Client):
 
     @app.route('/club/<club_id>/links/<link_id>', methods=['DELETE'])
     @authenticate_club_admin(supabase)
-    def delete_club_link(club_id, link_id):
+    def delete_club_link(club_id, link_id, auth_id):
         try:
             response = supabase.table('Club_link').delete().eq("club_id", club_id).eq("link_id", link_id).execute()
 
