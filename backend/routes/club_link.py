a6fa64b833c8066bdf6633f703afac2b4bdddc96
aylaurquizo <urquizo.a@northeastern.edu>
2024-11-06 18:21:45 -0500

    update_club_link

--- backend/routes/club_link.py
+++ backend/routes/club_link.py
@@ -5,7 +5,7 @@ from supabase import Client
 # Put club link routes here (creating, reading, updating, deleting)
 def ClubLinkRoutes(app: Flask, supabase: Client):
 
-    @app.route("/club/<club_id>/links/<link_id>", methods=["PUT"])
+    @app.route("/club/<club_id>/club_link/<link_id>", methods=["PUT"])
     @authenticate_club_admin(supabase)
     def update_club_link(club_id, link_id, admin_id):
         try:
