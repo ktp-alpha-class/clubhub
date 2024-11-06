e29022dfec78fe574b2e06026da3ceefcf43e9df
aylaurquizo <urquizo.a@northeastern.edu>
2024-11-06 18:15:25 -0500

    added authentication

--- backend/routes/club_link.py
+++ backend/routes/club_link.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from auth import authenticate_club_admin
 from supabase import Client
 
 # Put club link routes here (creating, reading, updating, deleting)
 def ClubLinkRoutes(app: Flask, supabase: Client):
 
     @app.route('/club/<club_id>/links/<link_id>', methods=['DELETE'])
+    @authenticate_club_admin(supabase)
     def delete_club_link(club_id, link_id):
         try:
-            response = supabase.table('club_link').delete().eq("club_id", club_id).eq("link_id", link_id).execute()
+            response = supabase.table('Club_link').delete().eq("club_id", club_id).eq("link_id", link_id).execute()
 
             if not response.data:
                 return jsonify({"error": "Club link not found"}), 404
