b8cb8fab423ff859502d6a3ca6d1f3e6df7042c8
cooper-ke <cooper.ke@northeastern.edu>
2024-11-07 21:58:36 -0500

    Figured out /user-me, brought in Seby's db design

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 from supabase import Client
 
+from backend.auth.authenticate_user import authenticate_user
+
 # Put routes for users (profile, following) here
 def UserRoutes(app: Flask, supabase: Client):
     # Get user profile data
-    @app.route("/user")
-    def get_examples():
-        profile_data = supabase.table("User").select("username").execute()
-        return jsonify(profile_data.data)
+    @app.route("/user-me")
+    @authenticate_user(supabase)
+    def get_me(user_id):
+        user_data = supabase.table("Users").select("*").execute()
+        return jsonify(user_data.data)
