f48fa15e4bb581f5fa58976ff463b2b8b8524736
cooper-ke <cooper.ke@northeastern.edu>
2024-11-07 22:08:47 -0500

    User profile data route

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,6 +1,26 @@
 from flask import Flask, jsonify
 from supabase import Client
 
+from backend.auth.authenticate_user import authenticate_user
+
 # Put routes for users (profile, following) here
 def UserRoutes(app: Flask, supabase: Client):
-    pass
+    # Get user profile data
+    @app.route("/user/<user_id>")
+    @authenticate_user(supabase)
+    def get_me(user_id):
+        try:
+            response = (
+                supabase.table("Users")
+                .select("*")
+                .eq("user_id", user_id)
+                .execute()
+            )
+
+            if not response.data:
+                return jsonify({"error": "User not found"}), 404
+
+            return jsonify({"user": response.data[0]}), 200
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
