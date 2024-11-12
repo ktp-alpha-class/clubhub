711d19c972412750592179c58075a30863f501f9
aylaurquizo <urquizo.a@northeastern.edu>
2024-11-12 13:55:25 -0500

    resolving comments

--- backend/routes/club_link.py
+++ backend/routes/club_link.py
@@ -5,7 +5,7 @@ from supabase import Client
 # Put club link routes here (creating, reading, updating, deleting)
 def ClubLinkRoutes(app: Flask, supabase: Client):
 
-    @app.route("/club/<club_id>/club_link/<link_id>", methods=["PUT"])
+    @app.route("/club/<club_id>/links/<link_id>", methods=["PUT"])
     @authenticate_club_admin(supabase)
     def update_club_link(club_id, link_id, admin_id):
         try:
@@ -15,7 +15,7 @@ def ClubLinkRoutes(app: Flask, supabase: Client):
                 return jsonify({"error": "No data provided"}), 400
 
             response = (
-                supabase.table("Club_link")
+                supabase.table("club_links")
                 .update(data)
                 .eq("club_id", club_id)
                 .eq("link_id", link_id)
@@ -25,6 +25,6 @@ def ClubLinkRoutes(app: Flask, supabase: Client):
             if not response.data:
                 return jsonify({"error": "Club link not found"}), 404
 
-            return jsonify({"message": "Club link updated successfully", "updated_club_link": response.data})
+            return response.data[0]
         except Exception as e:
-            return jsonify({"error": str(e)}), 400
+            return jsonify({"error": str(e)}), 500
