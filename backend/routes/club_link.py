9c8e5f20a09c5588f00e21de23187ef61a68a1f6
Michael Brennan <michael.brennancolorado@gmail.com>
2024-11-16 11:19:05 -0500

    club_link delete works

--- backend/routes/club_link.py
+++ backend/routes/club_link.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from auth import authenticate_club_admin
+from auth.authenticate_club_admin import authenticate_club_admin
 from supabase import Client
 
 # Put club link routes here (creating, reading, updating, deleting)
@@ -7,7 +7,7 @@ def ClubLinkRoutes(app: Flask, supabase: Client):
 
     @app.route('/club/<club_id>/links/<link_id>', methods=['DELETE'])
     @authenticate_club_admin(supabase)
-    def delete_club_link(club_id, link_id, auth_id):
+    def delete_club_link(club_id, link_id, admin_id):
         try:
             response = supabase.table('club_links').delete().eq("club_id", club_id).eq("link_id", link_id).execute()
 
