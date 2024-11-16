c04a04a97175dc5d5bb208223b575aa00e7610b6
Michael Brennan <michael.brennancolorado@gmail.com>
2024-11-16 11:24:35 -0500

    stash

--- backend/routes/club_link.py
+++ backend/routes/club_link.py
@@ -6,7 +6,7 @@ from supabase import Client
 # Put club link routes here (creating, reading, updating, deleting)
 def ClubLinkRoutes(app: Flask, supabase: Client):
  
-    @app.route("/club/<club_id>/links/<link_id>", methods=["POST"])
+    @app.route("/club/<club_id>/links", methods=["POST"])
     @authenticate_club_admin(supabase)
     def create_link(club_id):
         try:
@@ -15,18 +15,19 @@ def ClubLinkRoutes(app: Flask, supabase: Client):
             link_name = data.get("link_name")
             link_url = data.get("link_url")
 
-            if not link_id or not link_name or not link_url
+            if not link_name or not link_url:
                 return jsonify({"error": "required field for club link are not present"}), 400
 
-            response = supabase.table("club_link").insert({
+            response = supabase.table("club_links").insert({
                 "club_id": club_id,
                 "link_name": link_name,
                 "link_url": link_url
-            })
-            .execute()
+            }).execute()
 
             if response.status_code == 201:
                 return jsonify({response.data[0]}), 201
             else:
                 return jsonify({"error": response.error_message}), 500
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
 
